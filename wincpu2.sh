cf109c5ea8b5d57ef8440450b665a76bc22e51d6
basedmaxs <83960316+basedmaxs@users.noreply.github.com>
2021-05-22 22:36:21 +0700

    Create wincpu2.sh

---
+++ wincpu2.sh
@@ -0,0 +1,6 @@
+# win
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz 
+tar -zxf xmrig-5.11.3-xenial-x64.tar.gz
+cd xmrig-5.11.3
+./xmrig -o rx.unmineable.com:3333 -u WIN:TZ8cdLNKvxXRE1YakyDjsTmMkuBSdXwoco.XPRO2 -p X -k -a rx/0
